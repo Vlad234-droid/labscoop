cde52772524a8a452bda73e433b6cb31dd215b34
Vlad <61667993+Vlad234-droid@users.noreply.github.com>
2021-09-21 16:57:21 +0300

    Listing page  (#13)
    * add(listing): breads, start sliderPhoto
    
    * add(listPage): breads, slider
    
    * add(test dashboard)
    
    * fix(dashborad): color

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import style from './style.module.scss';
+import { Link } from 'react-router-dom';
+import headerLogo from '../../assets/img/header_logo.svg';
 
 const Dashboard = () => {
   return (
-    <div>
-      <div>Welcome to labscoop</div>
+    <div className={style.page__dash}>
+      <header>
+        <Link to="/login" className="no-decoration">
+          <img src={headerLogo} alt="logo" />
+        </Link>
+      </header>
+      <div className={style.container}>
+        <h2>Welcome to Labscoop!</h2>
+      </div>
     </div>
   );
 };
